48571b78c22c211d0f6bbc00147af8d9d57c95fd
Badarinath Venkatnarayansetty <badrinathvm@gmail.com>
2025-08-24 13:03:24 -0700

    Added home brew version swift

--- Formula/swift-version-mcp.rb
+++ Formula/swift-version-mcp.rb
@@ -1,15 +1,19 @@
 class SwiftVersionMcp < Formula
     desc "Swift version MCP"
     homepage "https://github.com/badrinathvm/SwiftVersionMCP"
-    url "https://github.com/badrinathvm/SwiftVersionMCP/archive/refs/heads/main.tar.gz"
+    url "https://github.com/badrinathvm/SwiftVersionMCP/archive/1.2.0.tar.gz"
     version "1.2.0"
     sha256 "70583451155641a75fb3f54d0ef8616200ec985f56e71ef3757affd24ad4659f"
   
     depends_on "swift"
   
     def install
-      system "swift", "build", "--configuration", "release"
-      bin.install ".build/release/SwiftVersionMCP" => "swift-version-mcp"
+      cd "SwiftVersionMCP-1.2.0" do
+        # Use Homebrew's Swift explicitly
+        swift_bin = Formula["swift"].opt_bin/"swift"
+        system swift_bin, "build", "--configuration", "release"
+        bin.install ".build/release/SwiftVersionMCP" => "swift-version-mcp"
+      end
     end
   
     test do
