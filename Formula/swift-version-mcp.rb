9b6a2eabbc2df8fcf3601947d31343097f0af1c9
Badarinath Venkatnarayansetty <badrinathvm@gmail.com>
2025-08-24 13:35:39 -0700

    Updated SHA

--- Formula/swift-version-mcp.rb
+++ Formula/swift-version-mcp.rb
@@ -3,7 +3,7 @@ class SwiftVersionMcp < Formula
     homepage "https://github.com/badrinathvm/SwiftVersionMCP"
     url "https://github.com/badrinathvm/SwiftVersionMCP/archive/refs/heads/main.tar.gz"
     version "1.2.0"
-    sha256 "70583451155641a75fb3f54d0ef8616200ec985f56e71ef3757affd24ad4659f"
+    sha256 "2764bc07cb037169c1180de354760d8c0f9548fe2f17fa0cb520ba6f7bb188c9"
   
     depends_on "swift"
   
