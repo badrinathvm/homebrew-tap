006052e31f597287d6f92cea8ef96d90026a442e
Badarinath Venkatnarayansetty <badrinathvm@gmail.com>
2025-08-25 17:18:55 -0700

    Updated formula to swift-dev-tools mcp

---
+++ Formula/swift-dev-tools-mcp.rb
@@ -0,0 +1,17 @@
+class SwiftDevToolsMcp < Formula
+    desc "Swift version MCP"
+    homepage "https://github.com/badrinathvm/swift-dev-tools-mcp.git"
+    url "https://github.com/badrinathvm/swift-dev-tools-mcp/archive/refs/heads/main.tar.gz"
+    sha256 "a957de1a45d209bea1bf03caf66e871284cbfc9d8575c3d55f0550aadd262ea8"
+  
+    depends_on "swift"
+  
+    def install
+      system "swift", "build", "--configuration", "release", "--disable-sandbox"
+      bin.install ".build/release/swift-dev-tools-mcp"
+    end
+  
+    test do
+      system "#{bin}/swift-dev-tools-mcp", "--help"
+    end
+  end
