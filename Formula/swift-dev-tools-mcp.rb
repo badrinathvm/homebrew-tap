81ac081e4cf4200e2fde8d9ca70f0c8ac530a9cd
Badarinath Venkatnarayansetty <badrinathvm@gmail.com>
2025-08-28 14:13:52 -0700

    Update swift-dev-tools-mcp.rb

--- Formula/swift-dev-tools-mcp.rb
+++ Formula/swift-dev-tools-mcp.rb
@@ -2,7 +2,7 @@ class SwiftDevToolsMcp < Formula
     desc "Swift Dev Tools MCP"
     homepage "https://github.com/badrinathvm/swift-dev-tools-mcp.git"
     url "https://github.com/badrinathvm/swift-dev-tools-mcp/archive/refs/heads/main.tar.gz"
-    version "1.0.0" 
+    version "2.0.0" 
     sha256 "c6c0912b3fb8e3a4a750465be7d7be51b9f8826992deff71a48fbc90e5025940"
   
     depends_on "swift"
