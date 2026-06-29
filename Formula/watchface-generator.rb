1666402f503b9c406c2db0fc5a16c22208c0c854
Badarinath Venkatnarayansetty <badrinathvm@gmail.com>
2026-06-29 08:50:11 -0700

    Update dthe hash

--- Formula/watchface-generator.rb
+++ Formula/watchface-generator.rb
@@ -7,7 +7,7 @@ class WatchfaceGenerator < Formula
     url "https://github.com/badrinathvm/WatchFaceGenerator/archive/refs/tags/v2.0.0.tar.gz"
     version "2.0.0"
 
-    sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+    sha256 "aec91238eb267d8b98e2bbda7cff5a200e2f218c4627ab69b1a88a5bbce28f82"
 
     def install
       # Install the Swift script as a standalone executable
