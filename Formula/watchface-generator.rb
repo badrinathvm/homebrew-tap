59847702a14bcf30f223707ea49a703d74e43e59
Badarinath Venkatnarayansetty <badrinathvm@gmail.com>
2026-06-28 23:50:27 -0700

    Removed depends on:macos

--- Formula/watchface-generator.rb
+++ Formula/watchface-generator.rb
@@ -7,12 +7,7 @@ class WatchfaceGenerator < Formula
     url "https://github.com/badrinathvm/WatchFaceGenerator/archive/refs/tags/v1.0.0.tar.gz"
     version "1.0.0"
 
-    # Update sha256 after tagging v1.0.0:
-    #   curl -L https://github.com/badrinathvm/WatchFaceGenerator/archive/refs/tags/v1.0.0.tar.gz | shasum -a 256
     sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
-
-    # Swift runtime required to execute the script
-    depends_on :macos
 
     def install
       # Install the Swift script as a standalone executable
